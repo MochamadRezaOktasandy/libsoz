af4770fc6b1b309c1aee2ea0372d5ac7913f3a47
MhmdAmmarNfldy <130427216+MhmdAmmarNfldy@users.noreply.github.com>
2023-05-19 20:26:46 +0700

    Mengisi data buku pada table buku

--- libsoz_db.sql
+++ libsoz_db.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.4.16-MariaDB - mariadb.org binary distribution
+-- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL Version:             12.4.0.6659
 -- --------------------------------------------------------
@@ -16,7 +16,7 @@
 
 
 -- Dumping database structure for libsoz
-CREATE DATABASE IF NOT EXISTS `libsoz` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `libsoz` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `libsoz`;
 
 -- Dumping structure for table libsoz.buku
@@ -30,9 +30,30 @@ CREATE TABLE IF NOT EXISTS `buku` (
   `lokasi` varchar(50) DEFAULT NULL,
   `sampul` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`buku_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table libsoz.buku: ~0 rows (approximately)
+-- Dumping data for table libsoz.buku: ~20 rows (approximately)
+REPLACE INTO `buku` (`buku_id`, `judul`, `pengarang`, `penerbit`, `tahun`, `kategori`, `lokasi`, `sampul`) VALUES
+	(1, 'Matahari', 'Tere Liye', 'Gramedia Pustaka Utama', 2019, 'Fiksi', NULL, NULL),
+	(2, 'Laskar Pelangi', 'Andrea Hirata', 'Bentang Pustaka', 2005, 'Fiksi', NULL, NULL),
+	(3, 'Bumi Manusia', 'Pramoedya Ananta Toer', 'Hasta Mitra', 1980, 'Fiksi', NULL, NULL),
+	(4, 'Ayat-Ayat Cinta', 'Habiburrahman El Shirazy', 'Republika Penerbit', 2004, 'Fiksi', NULL, NULL),
+	(5, 'Sang Pemimpi', 'Andrea Hirata', 'Bentang Pustaka', 2006, 'Fiksi', NULL, NULL),
+	(6, 'Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 'Bloomsbury Publishing', 1997, 'Fantasi', NULL, NULL),
+	(7, 'Gadis Pantai', 'Pramoedya Ananta Toer', 'Hasta Mitra', 1985, 'Fiksi', NULL, NULL),
+	(8, 'Dilan: Dia adalah Dilanku tahun 1990', 'Pidi Baiq', 'Dar! Mizan', 2014, 'Fiksi', NULL, NULL),
+	(9, 'Negeri 5 Menara', 'Ahmad Fuadi', 'Gramedia Pustaka Utama', 2009, 'Fiksi', NULL, NULL),
+	(10, 'Sang Alchemist', 'Paulo Coelho', 'HarperCollins', 1988, 'Fiksi', NULL, NULL),
+	(11, 'Ayah', 'Andrea Hirata', 'Bentang Pustaka', 2011, 'Fiksi', NULL, NULL),
+	(12, 'Lima Sekawan', 'Enid Blyton', 'Egmont Group', 1942, 'Petualangan', NULL, NULL),
+	(13, 'Perahu Kertas', 'Dee Lestari', 'Bentang Pustaka', 2009, 'Fiksi', NULL, NULL),
+	(14, 'Laut Bercerita', 'Leila S. Chudori', 'Kepustakaan Populer Gramedia', 2017, 'Fiksi', NULL, NULL),
+	(15, 'Sang Pencerah', 'Andrea Hirata', 'Bentang Pustaka', 2005, 'Fiksi', NULL, NULL),
+	(16, 'Tenggelamnya Kapal Van Der Wijck', 'Hamka', 'Balai Pustaka', 1938, 'Fiksi', NULL, NULL),
+	(17, 'Hujan', 'Tere Liye', 'Gramedia Pustaka Utama', 2014, 'Fiksi', NULL, NULL),
+	(18, 'Bumi', 'Tere Liye', 'Gramedia Pustaka Utama', 2004, 'Fiksi', NULL, NULL),
+	(19, 'Senja di Jakarta', 'Mochtar Lubis', 'Balai Pustaka', 1950, 'Fiksi', NULL, NULL),
+	(20, 'Sitti Nurbaya', 'Marah Rusli', 'Balai Pustaka', 1922, 'Fiksi', NULL, NULL);
 
 -- Dumping structure for table libsoz.member
 CREATE TABLE IF NOT EXISTS `member` (
@@ -43,9 +64,9 @@ CREATE TABLE IF NOT EXISTS `member` (
   `no_telp` varchar(20) DEFAULT NULL,
   `alamat` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`username`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table libsoz.member: ~0 rows (approximately)
+-- Dumping data for table libsoz.member: ~1 rows (approximately)
 REPLACE INTO `member` (`username`, `nama`, `tgl_lahir`, `email`, `no_telp`, `alamat`) VALUES
 	('1', 'ASDJKASHASDJK', '2023-05-01', 'asdasasasd', '1232121312123', 'asdasdasdasdasdasdasdas');
 
@@ -57,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `pinjam` (
   `username` varchar(50) DEFAULT NULL,
   `buku_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`pinjam_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table libsoz.pinjam: ~0 rows (approximately)
 
@@ -67,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` char(32) DEFAULT NULL,
   `role` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table libsoz.user: ~5 rows (approximately)
 REPLACE INTO `user` (`username`, `password`, `role`) VALUES
